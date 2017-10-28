9788149b3dfb435550e45d7dfe60b2211756733f
wastefactory <krischukytheodore@gmail.com>
2017-10-28 23:24:58 +0800

    added animate.css

--- src/components/Classes.vue
+++ src/components/Classes.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row class-container">
     <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3" v-for="classData in classes">
-      <div class="box" @click="classDetails(classData)">{{classData.name}}</div>
+      <div class="box animated slideInUp" @click="classDetails(classData)">{{classData.name}}</div>
     </div>
     <class-details />
     <vue-progress-bar></vue-progress-bar>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'flexboxgrid/css/flexboxgrid.min.css'
+import 'animate.css/animate.min.css'
 
 import Vue from 'vue'
 import App from './App'
